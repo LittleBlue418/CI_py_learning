47cf51b33ede981df5221619a92816e6fb59f87b
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-21 20:06:34 +0100

    test number five passed

--- evens.py
+++ evens.py
@@ -11,17 +11,17 @@ def even_number_of_evens(number):
         if n % 2 == 0:
             even_numbers.append(n)
 
-    if len(even_numbers) < 2:
-        return False
-    else:
+    if len(even_numbers) > 0 and len(even_numbers) % 2 == 0:
         return True
+    else:
+        return False
 
 
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2]) == False, "One even number"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
-#assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even number"
-#assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
+assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even number"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three even"
 #assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four even"
 #assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
 
