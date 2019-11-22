154015447d4b02f82bec70bb4cf38cd33650270b
LittleBlue418 <hollyathomas88@gmail.com>
2019-11-22 11:44:51 +0100

    writing more examples

--- byotest.py
+++ byotest.py
@@ -4,4 +4,9 @@ def number_of_evens(numbers):
 def tests_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
-test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+def tests_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
+
+tests_not_equal(number_of_evens([1,2,3,4,5]), 2)
+#tests_are_equal(number_of_evens([1,2,3,4,5]), 2)
+print("All tests padssed")
